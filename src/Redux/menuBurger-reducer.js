9dcc23ce335640f4f97a9a1b86d3a0dccccd8b47
Alexsandr Zolotarev <380788-500@mail.ru>
2024-11-21 21:30:29 +0300

    Fix Burger?

--- src/Redux/menuBurger-reducer.js
+++ src/Redux/menuBurger-reducer.js
@@ -8,8 +8,8 @@ let initialState = {
 const MenuBurgerReducer = (state = initialState, action) => {
   switch (action.type) {
     case TOGGLE_BURGER: {
-      if(state.bool) document.body.style.overflow = 'hidden';
-      else document.body.style.overflow = 'auto';
+      if(state.bool) document.body.style.overflow = 'auto';
+      else document.body.style.overflow = 'hidden';
       state.bool = !state.bool;
       return {...state};
     }
